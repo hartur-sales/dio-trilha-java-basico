fb75e86d205fc61d900dd900c319127f9099c6fa
Hartur <harturzinho10@gmail.com>
2024-07-01 21:43:58 -0300

    examples from the course

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 1500.00;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // e pra nao mudar? a gente usa as constantes, que no caso é socar o FINAL
+
+        int numero = 5;
+        numero = 15;
+        System.out.println("Numero: " + numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        String meuNome = "Hartur";
+
     }
 }
