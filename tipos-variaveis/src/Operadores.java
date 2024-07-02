383a203ac181193fd44ee4bbd8d3ff85e19ee031
Hartur <harturzinho10@gmail.com>
2024-07-02 19:33:58 -0300

    union of words

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        // Aqui foi basicamente explorando concatenação, e como era muita coisa, e eu
+        // não conseguia copiar como ele, eu só fui printando
+    }
+}
